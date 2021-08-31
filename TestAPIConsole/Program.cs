0b8277ce5135ca7531cb8180834fe1f9bcc7816d
Matthew-Lukowski <78889270+Matthew-Lukowski@users.noreply.github.com>
2021-08-31 16:56:33 -0500

    API Test Program

---
+++ TestAPIConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using static System.Console;
+using System.Net;
+using System.ComponentModel;
+
+
+namespace TestAPIConsole {
+    class Program {
+
+        static void Main() {
+            WriteLine("Enter a location");
+
+            callurl(ReadLine());
+
+            Write("Press any key to end");
+            ReadKey();
+        }
+
+        public static void callurl(string search_location) {
+            var client = new WebClient();
+            var content = client.DownloadString("http://api.weatherapi.com/v1/current.json?key="+ Key.API_KEY +"&q="+search_location+"&aqi=no");
+            LocationRoot loc = JsonConvert.DeserializeObject<LocationRoot>(content);
+
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(loc.Location)) {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(loc.Location);
+                WriteLine($"{name}: {value}");
+            }
+
+            WriteLine();
+
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(loc.CurrentTemp)) {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(loc.CurrentTemp);
+                WriteLine("{0}: {1}", name, value);
+            }
+
+
+
+        }
+    }
+    
+}
---
+++ TestAPIConsole/WeatherRequestObject.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+public class LocationRoot {
+    [JsonProperty("location")]
+    public Location Location { get; set; }
+    [JsonProperty("current")]
+    public CurrentTemp CurrentTemp { get; set; }
+}
+
+public class Location {
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    [JsonProperty("region")]
+    public string Region { get; set; }
+    [JsonProperty("country")]
+    public string Country { get; set; }
+    [JsonProperty("lat")]
+    public double Latitude { get; set; }
+    [JsonProperty("lon")]
+    public double Longitude { get; set; }
+    [JsonProperty("tz_id")]
+    public string Timezone { get; set; }
+    [JsonProperty("localtime")]
+    public string Localtime { get; set; }
+}
+
+public class CurrentTemp {
+    [JsonProperty("last_updated")]
+    public string Last_Updated_Time { get; set; }
+    [JsonProperty("temp_c")]
+    public double Temp_C { get; set; }
+    [JsonProperty("temp_f")]
+    public double Temp_F { get; set; }
+    [JsonProperty("uv")]
+    public double UV_Index { get; set; }
+}
+
+
+
